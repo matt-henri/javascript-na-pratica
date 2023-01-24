cefa175acdc42fc6099fdd3ac6dffe0923cedff8
Mateus Henrique <mathmarkub@gmail.com>
2023-01-24 13:50:44 -0300

    Update script.js

--- 007-multiplos-4-5/script.js
+++ 007-multiplos-4-5/script.js
@@ -2,7 +2,7 @@
 
     function calcular() {
         let multiplos3 = 0
-    let multiplos5 = 0
+        let multiplos5 = 0
     for (i = 0; i < 100; i++){
         multiplos3 += (i % 3 == 0) ? i : 0;
         multiplos5 += (i % 5 == 0) ? i : 0;
@@ -18,4 +18,4 @@
         document.getElementById('resultado').textContent = ''
         document.getElementById('resultado2').textContent = ''
         document.getElementById('resultado3').textContent = ''
-    }
+    }      
