f2e91b0856c145243e2ecd6ef2efb76401af5213
Mateus Henrique <mathmarkub@gmail.com>
2023-01-24 10:38:30 -0300

    att

--- imc-calculo/script.js
+++ imc-calculo/script.js
@@ -6,17 +6,19 @@ function calcIMC() {
     const peso = document.getElementById('peso').value
     const altura = document.getElementById('altura').value
 
-    let imc = (peso / (altura * altura))
+    let imc = (peso / (altura * altura)).toFixed(1)
 
-    
+    if (nome !== '' && peso !== '' && altura !== '') {
         if (imc < 17.9) {
-            document.getElementById('resultado').textContent = 'vc esta abaixo do peso'
+            document.getElementById('resultado').textContent = `${nome} seu IMC é ${imc}, você está abaixo do peso`
         } else if (imc < 24.9) {
-            document.getElementById('resultado').textContent = 'vc esta no peso ideal'
+            document.getElementById('resultado').textContent = `${nome} seu IMC é ${imc}, e você está no peso ideal`
         } else if (imc < 29.9) {
-            document.getElementById('resultado').textContent = 'vc esta levemente acima do peso'
+            document.getElementById('resultado').textContent = `${nome} seu IMC é ${imc}, você está levemente acima do peso`
         } else {
-            document.getElementById('resultado').textContent = 'vc esta com obesidade'
+            document.getElementById('resultado').textContent = `${nome} seu IMC é ${imc}, você está com obesidade`
+        } } else {
+            document.getElementById('resultado').textContent = 'Preencha todos os campos'
         }
     
 }
