fdf5019f3439413f4018f46ca4cb42fe31a6de91
Mateus Henrique <mathmarkub@gmail.com>
2023-01-24 21:50:43 -0300

    att

--- 008-teste/script.js
+++ 008-teste/script.js
@@ -1,8 +1,12 @@
+
+
 function soma() {
     let num1 = document.getElementById('num1').value
     let num2 = document.getElementById('num2').value
-    
+
     num1 = parseInt(num1)
     num2 = parseInt(num2)
-    document.getElementById('resultado').textContent = 'O resultado da soma é: ' + (num1 + num2 )
+
+    document.getElementById('resultado').textContent = 'O resultado da soma é: ' + (num1 + num2)
 }
+
