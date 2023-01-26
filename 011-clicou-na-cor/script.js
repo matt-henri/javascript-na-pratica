3203b700eed768589e5505280c9f9415d6c4e81e
Mateus Henrique <mathmarkub@gmail.com>
2023-01-26 20:06:33 -0300

    att

--- 011-clicou-na-cor/script.js
+++ 011-clicou-na-cor/script.js
@@ -2,8 +2,21 @@ const azul = document.getElementById('azul')
 const verde = document.getElementById('verde')
 const amarelo = document.getElementById('amarelo')
 const vermelho = document.getElementById('vermelho')
+const resultado = document.getElementById('resultado')
 
-function azul() {
-    const resultado = document.getElementById('resultado')
-    resultado.textContent = 'teste'
-}
+
+azul.addEventListener('click', function() {
+    resultado.textContent = 'Você clicou no Azul'
+})
+
+verde.addEventListener('click', function() {
+    resultado.textContent = 'Você clicou no Verde'
+})
+
+amarelo.addEventListener('click', function() {
+    resultado.textContent = 'Você clicou no Amarelo'
+})
+
+vermelho.addEventListener('click', function() {
+    resultado.textContent = 'Você clicou no Vermelho'
+})
