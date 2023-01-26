7c4e22187c752ce79669c572b0c17f31e63c64d2
Mateus Henrique <mathmarkub@gmail.com>
2023-01-26 19:57:27 -0300

    att

---
+++ 011-clicou-na-cor/script.js
@@ -0,0 +1,9 @@
+const azul = document.getElementById('azul')
+const verde = document.getElementById('verde')
+const amarelo = document.getElementById('amarelo')
+const vermelho = document.getElementById('vermelho')
+
+function azul() {
+    const resultado = document.getElementById('resultado')
+    resultado.textContent = 'teste'
+}
