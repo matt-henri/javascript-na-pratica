f963cedc3fdaf6544e5ab7e39fd415cf7e84f8f4
matt-henrii <mathmarkub@gmail.com>
2023-01-29 12:51:53 -0300

    Update script.js

--- 010-contador-click/script.js
+++ 010-contador-click/script.js
@@ -1,17 +1,1 @@
-const mais = document.getElementById('mais')
-const menos = document.getElementById('menos')
-const p = document.getElementById('contador')
-
-
-let contador = 0
-
-p.textContent = contador
-
-mais.addEventListener('click',   function() {
-    p.textContent = ++contador
-})
-
-menos.addEventListener('click',   function() {
-    p.textContent = --contador
-})
-
+const 
