997fc1e570d38fd6621a8aeda4978fae9fe71da1
matt-henrii <mathmarkub@gmail.com>
2023-01-29 14:40:52 -0300

    Update script.js

--- 008-soma-adição/script.js
+++ 008-soma-adição/script.js
@@ -1,16 +1,16 @@
 const resultado = document.getElementById('resultado')
 
-    function soma() {
-        let num1 = document.getElementById('num1').value
-        let num2 = document.getElementById('num2').value
+function soma() {
+    let num1 = document.getElementById('num1').value
+    let num2 = document.getElementById('num2').value
 
-        if (num1 !== '' && num2 !== '') {
-            num1 = parseInt(num1)
-            num2 = parseInt(num2)
+    if (num1 !== '' && num2 !== '') {
+        num1 = parseInt(num1)
+        num2 = parseInt(num2)
 
-            resultado.textContent = 'O resultado da soma é: ' + (num1 + num2)
-        } else {
-            resultado.textContent = 'Preencha todos os campos'
-        }
+        resultado.textContent = 'O resultado da soma é: ' + (num1 + num2)
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos!'
     }
-
+}
