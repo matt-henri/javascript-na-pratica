a6fd1574d05b259b151ab3b7f83e618ec5b44cd9
Mateus Henrique <mathmarkub@gmail.com>
2023-01-31 19:31:26 -0300

    att

---
+++ 013-qual-maior-numero/script.js
@@ -0,0 +1,20 @@
+const resultado = document.getElementById('resultado')
+
+function soma() {
+    const numUm = document.getElementById('numUm').value
+    const numDois = document.getElementById('numDois').value
+
+    var numeroUm = parseInt(numUm)
+    var numeroDois = parseInt(numDois)
+
+    if (numUm !== '' && numDois !== '') {
+        if (numeroUm > numeroDois) {
+            resultado.textContent = 'O número da Primeira Caixa é maior'
+        } else {
+            resultado.textContent = 'O número da Segunda Caixa é maior'
+        }
+    } else {
+        resultado.textContent = 'Favor preencha todos os campos!'
+    }
+
+}
