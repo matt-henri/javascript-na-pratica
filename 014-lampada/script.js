c1a6028e4bac219b1e54a5adc8a3e8d364c3b5d3
Mateus Henrique <mathmarkub@gmail.com>
2023-02-03 20:36:24 -0300

    Update script.js

--- 014-lampada/script.js
+++ 014-lampada/script.js
@@ -2,10 +2,24 @@ const turnOn = document.getElementById('turnOn')
 const turnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
 
-
-function lampOn() {
-    img.src = './img/ligada.jpg'
+function isLampBroken() {
+    return lamp.src.indexOf ( 'quebrada' ) > -1
 }
 
+function lampBroken() {
+    lamp.src = './img/quebrada.jpg'
+}
 
-turnOn.addEventListener('clickup', lampOn)
+function lampOn() {
+    lamp.src = './img/ligada.jpg'
+}
+
+function lampOf() {
+    lamp.src = './img/desligada.jpg'
+}
+
+turnOn.addEventListener('click', lampOn)
+turnOff.addEventListener('click', lampOf)
+lamp.addEventListener('mouseover', lampOn)
+lamp.addEventListener('mouseout', lampOf)
+lamp.addEventListener('dblclick', lampBroken)
